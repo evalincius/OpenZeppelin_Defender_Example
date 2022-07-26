f257bf906d2f6cfdcca14731539ec5e6b808cc16
evalincius <edgaras.valincius@qa.com>
2022-07-26 07:15:26 +0100

    fix react warning

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 
 function MyApp({ Component, pageProps }) {
-  return (<div class="bg-gradient-radial h-screen">
-             <div class="container">
+  return (<div className="bg-gradient-radial h-screen">
+             <div className="container">
               <nav className="md:flex py-8">
 
-                <div class="flex-1 m-auto py-2">
-                  <div class="flex justify-between w-80 m-auto"> 
+                <div className="flex-1 m-auto py-2">
+                  <div className="flex justify-between w-80 m-auto"> 
                     <a href="https://discord.com/" target="_blank"> 
                     <img src="https://www.skillsblock.mtcserver.com/wp-content/uploads/2022/07/soc1.png"></img>
                       {/* <span>Discord</span> */}
@@ -48,16 +48,16 @@ function MyApp({ Component, pageProps }) {
                   </div>
                 </div>
 
-                <div class="flex-1 flex justify-center py-2">
+                <div className="flex-1 flex justify-center py-2">
                   
                     <img src="https://www.skillsblock.mtcserver.com/wp-content/uploads/2022/07/logo.png"></img>
                   
                 </div>
                
                 
-                <div class="flex-1 flex justify-center">
+                <div className="flex-1 flex justify-center">
 
-                <div class="flex justify-between m-auto m-auto">
+                <div className="flex justify-between m-auto m-auto">
                     {/* <Link href="/">
                       <a href="#_" class="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-full">
                           <span class="w-full h-full bg-gradient-to-r from-[#df2484] to-[#e83852] absolute"></span>
@@ -68,26 +68,26 @@ function MyApp({ Component, pageProps }) {
                           </span>
                       </a>
                     </Link> */}
-                    <div class="px-2">
+                    <div className="px-2">
                       <Link href="/create-item">
-                        <a href="#_" class="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-full">
-                            <span class="w-full h-full bg-gradient-to-r from-[#df2484] to-[#e83852] absolute"></span>
-                            <span class="relative px-6 py-3 bg-gray-900 rounded-full">
-                              <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-gradient-to-r from-[#df2484] to-[#e83852] opacity-[3%] "></span>
-                              <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-1000 ease-in-out  -translate-x-56 -translate-y-24 bg-gradient-to-br from-[#df2484] to-[#e83852] opacity-100 group-hover:-translate-x-8"></span>
-                              <span class="relative w-full text-left text-white transition-colors duration-800 ease-in-out">Mint</span>
+                        <a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-full">
+                            <span className="w-full h-full bg-gradient-to-r from-[#df2484] to-[#e83852] absolute"></span>
+                            <span className="relative px-6 py-3 bg-gray-900 rounded-full">
+                              <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-gradient-to-r from-[#df2484] to-[#e83852] opacity-[3%] "></span>
+                              <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-1000 ease-in-out  -translate-x-56 -translate-y-24 bg-gradient-to-br from-[#df2484] to-[#e83852] opacity-100 group-hover:-translate-x-8"></span>
+                              <span className="relative w-full text-left text-white transition-colors duration-800 ease-in-out">Mint</span>
                             </span>
                         </a>
                       </Link>
                     </div>
-                    <div class="px-2">
+                    <div className="px-2">
                       <Link href="/my-assets" >
-                        <a href="#_" class="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-full">
-                            <span class="w-full h-full bg-gradient-to-r from-[#df2484] to-[#e83852] absolute"></span>
-                            <span class="relative px-6 py-3 bg-gray-900 rounded-full">
-                              <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-gradient-to-r from-[#df2484] to-[#e83852] opacity-[3%]"></span>
-                              <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out  -translate-x-56 -translate-y-24 bg-gradient-to-br from-[#df2484] to-[#e83852] opacity-100 group-hover:-translate-x-8 "></span>
-                              <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out">My NFTs</span>
+                        <a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-full">
+                            <span className="w-full h-full bg-gradient-to-r from-[#df2484] to-[#e83852] absolute"></span>
+                            <span className="relative px-6 py-3 bg-gray-900 rounded-full">
+                              <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-gradient-to-r from-[#df2484] to-[#e83852] opacity-[3%]"></span>
+                              <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out  -translate-x-56 -translate-y-24 bg-gradient-to-br from-[#df2484] to-[#e83852] opacity-100 group-hover:-translate-x-8 "></span>
+                              <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out">My NFTs</span>
                             </span>
                         </a>
                       </Link>
--- pages/create-item.js
+++ pages/create-item.js
@@ -56,10 +56,13 @@ export default function CreateItem() {
 
       async function listNFTForSale() {
         const url = await uploadToIPFS()
+        console.log('Uploaded!')
 
         // const credentials = { apiKey: "9qYN4Znuin9eTNk62uYmN6nhc6hr6eXQ", apiSecret: "4iXG5yj9o2ZQicRHKA2ZF2yZgSB2HfAPwFcqJNnm14Jsx2LqrRsHod8Et3iUryTt" };
         // const provider = new DefenderRelayProvider(credentials);
         // const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
+
+
 
 
         const web3Modal = new Web3Modal()
@@ -83,29 +86,29 @@ export default function CreateItem() {
       return (
         <div className="flex justify-center">
           <div className="w-5/6 md:w-1/2">
-            <div class="py-2 ">
-                <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-                <input type="text" id="small-input" class="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={e => updateFormInput({ ...formInput, name: e.target.value })}/>
+            <div className="py-2 ">
+                <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
+                <input type="text" id="small-input" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={e => updateFormInput({ ...formInput, name: e.target.value })}/>
             </div>
 
-            <div class="py-2">
-                <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
-                <input type="text" id="small-input" class="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={e => updateFormInput({ ...formInput, description: e.target.value })}/>
+            <div className="py-2">
+                <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
+                <input type="text" id="small-input" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={e => updateFormInput({ ...formInput, description: e.target.value })}/>
             </div>
 
-            <div class="py-2">
-                <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Value</label>
-                <input type="text" id="small-input" class="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={e => updateFormInput({ ...formInput, price: e.target.value })}/>
+            <div className="py-2">
+                <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Value</label>
+                <input type="text" id="small-input" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={e => updateFormInput({ ...formInput, price: e.target.value })}/>
             </div>
 
-            <div class="flex justify-center items-center w-full py-2">
-                <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                    <div class="flex flex-col justify-center items-center pt-5 pb-6">
-                        <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+            <div className="flex justify-center items-center w-full py-2">
+                <label htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                        <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                     </div>
-                    <input id="dropzone-file" type="file" class="hidden" onChange={onChange}/>
+                    <input id="dropzone-file" type="file" className="hidden" onChange={onChange}/>
                 </label>
             </div> 
 
@@ -116,13 +119,13 @@ export default function CreateItem() {
               )
             }
 
-            <div class="flex justify-center py-">
-              <button onClick={listNFTForSale} class="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-full">
-                  <span class="w-full h-full bg-gradient-to-r from-[#df2484] to-[#e83852] absolute"></span>
-                  <span class="relative px-6 py-3 bg-gray-900 rounded-full">
-                    <span class="absolute left-0 top-0 bg-gradient-to-r from-[#df2484] to-[#e83852] opacity-[3%]"></span>
-                    <span class="absolute top-0 left-0 w-72 h-48 transition-all duration-1000 ease-in-out -translate-x-72 -translate-y-24 bg-gradient-to-br from-[#df2484] to-[#e83852] opacity-100 group-hover:-translate-x-8 "></span>
-                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out">Mint My Certificate NFT</span>
+            <div className="flex justify-center py-">
+              <button onClick={listNFTForSale} className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-full">
+                  <span className="w-full h-full bg-gradient-to-r from-[#df2484] to-[#e83852] absolute"></span>
+                  <span className="relative px-6 py-3 bg-gray-900 rounded-full">
+                    <span className="absolute left-0 top-0 bg-gradient-to-r from-[#df2484] to-[#e83852] opacity-[3%]"></span>
+                    <span className="absolute top-0 left-0 w-72 h-48 transition-all duration-1000 ease-in-out -translate-x-72 -translate-y-24 bg-gradient-to-br from-[#df2484] to-[#e83852] opacity-100 group-hover:-translate-x-8 "></span>
+                    <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out">Mint My Certificate NFT</span>
                   </span>
               </button>
             </div>
